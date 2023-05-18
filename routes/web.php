76803b049f8cca1eb525a9467e6636f27c87e423
daniel <daniel@oikarainen.se>
2023-05-18 22:02:52 +0200

    updated app_url for laravel and added routes

--- routes/web.php
+++ routes/web.php
@@ -26,3 +26,13 @@ Route::get('forge', function () {
 Route::get('adapt', function () {
     return "Adapt is the way";
 });
+
+
+Route::get('foo', function () {
+    return "FooBar FooBar FooBar FooBar FooBar FooBar ";
+});
+
+
+Route::get('dance', function () {
+    return "cha cha cha cha cha ";
+});
